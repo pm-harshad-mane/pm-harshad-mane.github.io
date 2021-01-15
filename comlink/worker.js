a3c60f86cae4856c9dd3f4b778e3abb9b7864f06
Harshad Mane <harshad.mane@pubmatic.com>
2021-01-14 16:45:39 -0800

    Update worker.js

--- comlink/worker.js
+++ comlink/worker.js
@@ -2,7 +2,7 @@ importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
 // importScripts("../../../dist/umd/comlink.js");
  
 const obj = {
-  counter: 0,
+  counter: 100,
   inc() {
     this.counter++;
   },
