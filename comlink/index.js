7d4cbc0a532267e30d532681fad05404574e3392
Harshad Mane <harshad.mane@pubmatic.com>
2021-01-14 16:49:19 -0800

    Update index.js

--- comlink/index.js
+++ comlink/index.js
@@ -1,6 +1,13 @@
 import * as Comlink from "https://unpkg.com/comlink/dist/esm/comlink.mjs";
 async function init() {
   const worker = new Worker("worker.js");
+  
+  Comlink.expose({
+     methodOnMainThread(m){
+         alert(m)
+    }
+  }, worker);
+  
   // WebWorkers use `postMessage` and therefore work with Comlink.
   const obj = Comlink.wrap(worker);
   alert(`Counter:: ${await obj.counter}`);
