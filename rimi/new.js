cebefce72e113a9b705b9aecadd2e25567f67761
Harshad Mane <harshad.mane@pubmatic.com>
2021-10-17 19:43:56 -0700

    Add files via upload

--- rimi/new.js
+++ rimi/new.js
@@ -52,6 +52,12 @@
 			canvas: true
 		});
 
+		stream.getTracks().forEach(function(track) {
+	        if (track.readyState == 'live' && track.kind === 'video') {
+	            track.stop();
+	        }
+	    });
+
 		document.querySelector("#photo").src = photo.toDataURL('image/jpeg');
 		carousel.to(2);
 	}
@@ -87,9 +93,63 @@
 	}
 
 	actionStartReading.onclick = function(){
-		var msg = new SpeechSynthesisUtterance();
-		msg.text = document.querySelector("#log").innerHTML;
-		window.speechSynthesis.speak(msg);
+		var utterance = new SpeechSynthesisUtterance();
+		utterance.text = document.querySelector("#log").innerHTML;
+		utterance.onboundary = onboundaryHandler;
+		window.speechSynthesis.speak(utterance);
+	}
+
+
+	// code to highlihght the text
+	function onboundaryHandler(event){
+	    var textarea = document.getElementById('log');
+	    var value = textarea.value;
+	    var index = event.charIndex;
+	    var word = getWordAt(value, index);
+	    var anchorPosition = getWordStart(value, index);
+	    var activePosition = anchorPosition + word.length;
+	    
+	    textarea.focus();
+	    
+	    if (textarea.setSelectionRange) {
+	       textarea.setSelectionRange(anchorPosition, activePosition);
+	    }
+	    else {
+	       var range = textarea.createTextRange();
+	       range.collapse(true);
+	       range.moveEnd('character', activePosition);
+	       range.moveStart('character', anchorPosition);
+	       range.select();
+	    }
+	};
+
+	// Get the word of a string given the string and index
+	function getWordAt(str, pos) {
+	    // Perform type conversions.
+	    str = String(str);
+	    pos = Number(pos) >>> 0;
+
+	    // Search for the word's beginning and end.
+	    var left = str.slice(0, pos + 1).search(/\S+$/),
+	        right = str.slice(pos).search(/\s/);
+
+	    // The last word in the string is a special case.
+	    if (right < 0) {
+	        return str.slice(left);
+	    }
+	    
+	    // Return the word, using the located bounds to extract it from the string.
+	    return str.slice(left, right + pos);
+	}
+
+	// Get the position of the beginning of the word
+	function getWordStart(str, pos) {
+	    str = String(str);
+	    pos = Number(pos) >>> 0;
+
+	    // Search for the word's beginning
+	    var start = str.slice(0, pos + 1).search(/\S+$/);
+	    return start;
 	}
 
 })();
