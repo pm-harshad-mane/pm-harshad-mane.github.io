2eb2e4254d6f2071ebf9a7797a1da5090f71d860
Harshad Mane <harshad.mane@pubmatic.com>
2026-02-04 14:26:12 -0800

    Create social.js

---
+++ dehaati-2/social.js
@@ -0,0 +1,10 @@
+var GoogleReviewWidget = {
+    rating: 5,
+    reviews: 56,
+    url: "https://maps.app.goo.gl/g7Q5voc2tPtrQvqN8"
+}
+
+var InstagramWidget = {
+    followers: "99",
+    url: "https://www.instagram.com/dehaati_indiancuisine/"
+}
