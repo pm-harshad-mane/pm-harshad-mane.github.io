70e5fbe4448effdf3441babe7fdd8b88d0e5ac07
Harshad Mane <harshad.mane@pubmatic.com>
2026-08-14 14:02:07 -0700

    Create favorites.js

---
+++ my-favourites/favorites.js
@@ -0,0 +1,7 @@
+const FAVORITES = [
+  "tt0133093", // The Matrix
+  "tt0903747", // Breaking Bad
+  "tt0944947", // Game of Thrones
+  "tt0111161", // The Shawshank Redemption
+  "tt2861424", // Rick and Morty
+];
